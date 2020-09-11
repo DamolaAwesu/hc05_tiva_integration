227d8e59d6b016adda2578af3f9a3194c5087ddb
Damola Awesu <62110318+DamolaAwesu@users.noreply.github.com>
2020-09-11 02:37:50 +0200

    BSP Header File
    Contains function prototypes and macros for configuring peripherals

---
+++ bsp.h
@@ -0,0 +1,19 @@
+#ifndef __BSP_H__
+#define __BSP_H__
+
+/* Board Support Package for the EK-TM4C123GXL board */
+
+/* system clock setting [Hz] */
+#define SYS_CLOCK_HZ 16000000U
+
+/* on-board LEDs */
+#define LED_RED   (1U << 1)
+#define LED_BLUE  (1U << 2)
+#define LED_GREEN (1U << 3)
+
+/* External Variables */
+extern uint32_t value;
+
+//void delay(uint32_t value);
+
+#endif // __BSP_H__
